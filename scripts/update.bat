76ca49cdd3aaa8544df0c311f0f339740bd5ac55
Devil4ngle <isaakpocchiesa@gmail.com>
2024-02-09 10:54:18 +0100

    Update Release

--- scripts/update.bat
+++ scripts/update.bat
@@ -1,8 +1,8 @@
 @echo off
 timeout /t 1 /nobreak 
 echo Fetching from Git...
-"%CD%\scripts\git\bin\git.exe" gc
-"%CD%\scripts\git\bin\git.exe" fetch
-echo n | "%CD%\scripts\git\bin\git.exe" reset origin/release --hard
+"%CD%\scripts\git\bin\git.exe" init
+"%CD%\scripts\git\bin\git.exe" remote add origin https://github.com/Devil4ngle/squadmortar-release.git
+echo n | "%CD%\scripts\git\bin\git.exe" reset origin --hard
 echo Script is up to date. Press any key to close.
 pause
