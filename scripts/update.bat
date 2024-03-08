c1c6a92fe5d3d171fa45d8d4de4c1c1c908b9e8e
Devil4ngle <isaakpocchiesa@gmail.com>
2024-03-08 13:33:46 +0100

    Update Release

---
+++ scripts/update.bat
@@ -0,0 +1,17 @@
+@echo off
+timeout /t 2 /nobreak 
+echo Fetching from Git...
+echo Current directory: %CD%
+
+"%CD%\git\bin\git.exe" init
+rem Check if remote origin exists
+"%CD%\git\bin\git.exe" remote -v | find "origin" > nul
+if %errorlevel% neq 0 (
+    echo Adding remote origin...
+    "%CD%\git\bin\git.exe" remote add origin https://github.com/Devil4ngle/squadmortar-release.git
+) else (
+    echo Remote origin already exists.
+)
+"%CD%\git\bin\git.exe" fetch
+echo Script is up to date. Press any key to close.
+pause
